76e17a57240c4eb11298a4ff74fb9a2e347eef35
unheroic-sam <unheroicmail@gmail.com>
2018-10-24 20:09:09 -0400

    updated signin/resgiter

--- tigerbook-react/src/App.js
+++ tigerbook-react/src/App.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
-import SignIn from './components/SignIn/signIn'
-import Register from './components/Register/Register'
+import SignIn from './components/SignIn/signIn';
+import Register from './components/Register/Register';
+import Home from './components/Home/Home';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-      	<SignIn />
-      	<Register />
-      </div>
-    );
-  }
+  
+	constructor() {
+		super()
+		this.state = {
+			isSignedIn: false,
+			route: 'signIn'
+		}
+	}
+
+	onRouteChange = (route) => {
+		if(route === 'signOut') {
+			this.setState({isSignedIn: false})
+		} else if (route === 'home') {
+			this.setState({isSignedIn: true})
+		}
+		this.setState({route: route});
+	}
+  	
+  	render() {
+    	const { isSignedIn, route } = this.state;
+    	return (
+      		<div>
+      			{ route === 'home'
+      				?<div>
+      					<Home />
+      				</div>
+      			:(
+      				this.state.route === 'signIn'
+      					?<SignIn onRouteChange={this.onRouteChange} />
+      					:<Register onRouteChange={this.onRouteChange} />
+      			)
+      		}
+      		</div>
+    	);
+  	};
 }
 
 export default App;
