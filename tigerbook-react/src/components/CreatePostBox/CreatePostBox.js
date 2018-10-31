f73b75e2762eaab652900ec3279c427cd4e00f8b
Elliott Scott <escott4@comcast.net>
2018-10-30 20:10:24 -0400

    Added create post box folder

---
+++ tigerbook-react/src/components/CreatePostBox/CreatePostBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import './CreatePostBox.css';
+
+
+const CreatePostBox = () => {
+	
+	return(
+		<div> 
+			<h1>Post</h1>
+		</div>
+	)
+
+}
+
+export default CreatePostBox;
